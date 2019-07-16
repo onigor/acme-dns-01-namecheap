b55b6998ba0433b3ef1af36e139d0f969a112582
nyaundi brian <danleyb2@gmail.com>
2019-07-16 18:04:09 +0300

    update tests to match api changes

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-// https://git.coolaj86.com/coolaj86/acme-challenge-test.js
-var tester = require('acme-challenge-test');
+// https://git.rootprojects.org/root/acme-dns-01-test.js
+var tester = require('acme-dns-01-test');
 
 // Usage: node ./test.js example.com xxxxxxxxx
 var zone = process.argv[2];
@@ -10,39 +10,21 @@ var zone = process.argv[2];
 var challenger = require('./index.js').create({
     apiUser:process.argv[3],
 	apiKey : process.argv[4],
-    username: process.argv[5]||process.argv[3]
+    clientIp:process.argv[5],
+    username: process.argv[6]||process.argv[3]
 });
 
 
 // The dry-run tests can pass on, literally, 'example.com'
 // but the integration tests require that you have control over the domain
-var domain = zone;
-
 tester
-	.test('dns-01', domain, challenger)
-	.then(function() {
-		console.info('PASS', domain);
-    ///*
-		domain = 'foo.' + zone;
-
-		return tester
-			.test('dns-01', domain, challenger)
-			.then(function() {
-				console.info('PASS', domain);
-			})
-			.then(function() {
-				domain = '*.foo.' + zone;
-
-				return tester.test('dns-01', domain, challenger).then(function() {
-					console.info('PASS', domain);
-				});
-			});
-      //*/
-	})
-	.catch(function(e) {
-        console.info('ERROR', domain);
-
+	.testZone('dns-01', zone, challenger)
+    .then(function() {
+        console.info('PASS', zone);
+    })
+    .catch(function(e) {
+        console.info('FAIL', zone);
         console.error(e.message);
-		console.error(e.stack);
-	});
+        console.error(e.stack);
+    });
 
